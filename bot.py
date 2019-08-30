022cb6856d4881da63c9ba142314a09358a9a0be
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-08-30 04:05:28 -0400

    add 5x skull matching logic

--- bot.py
+++ bot.py
@@ -17,6 +17,9 @@ gemScreenLocations = {}
 def in_bounds(x, y):
 	return x >= 0 and x < GRID_WIDTH and y >= 0 and y < GRID_HEIGHT
 
+def can_match(a, b):
+	return (a == b) or (a == "5" and b == "s") or (a == "s" and b == "5")
+
 while(True):
 	# try to find the top left edge of the grid
 	topLeftLocations = pyautogui.locateOnScreen("Assets/topleft.PNG", confidence = .99)
@@ -45,67 +48,69 @@ while(True):
 				grid[gridY][gridX] = gemImageName[0]
 				gemGridLocations[gemImageName] += [{"y": gridY, "x": gridX}]
 
+		for i in grid:
+			print(i)
+
 		moves = collections.defaultdict(list)
 		for y in range(1, GRID_HEIGHT):
 			for x in range(1, GRID_WIDTH):
 				if (grid[y][x] != "?"): # brute force a detect 3 move, inspired by https://github.com/akleemans/bejeweled-bot/blob/master/bejeweled-bot.py
 					# (from_x, from_y, to_x, to_y)
-					if (grid[y][x] == grid[y][x-1]):
-						if (in_bounds(x+1, y-1) and grid[y][x] == grid[y-1][x+1]):
+					if (can_match(grid[y][x], grid[y][x-1])):
+						if (in_bounds(x+1, y-1) and can_match(grid[y][x], grid[y-1][x+1])):
 							moves[(x+1, y-1, x+1, y)].append(grid[y][x])
-						if (in_bounds(x+2, y) and grid[y][x] == grid[y][x+2]):
+						if (in_bounds(x+2, y) and can_match(grid[y][x], grid[y][x+2])):
 							moves[(x+2, y, x+1, y)].append(grid[y][x])
-						if (in_bounds(x+1, y+1) and grid[y][x] == grid[y+1][x+1]):
+						if (in_bounds(x+1, y+1) and can_match(grid[y][x], grid[y+1][x+1])):
 							moves[(x+1, y+1, x+1, y)].append(grid[y][x])
 
-						if (in_bounds(x-2, y-1) and grid[y][x] == grid[y-1][x-2]):
+						if (in_bounds(x-2, y-1) and can_match(grid[y][x], grid[y-1][x-2])):
 							moves[(x-2, y-1, x-2, y)].append(grid[y][x])
-						if (in_bounds(x-3, y) and grid[y][x] == grid[y][x-3]):
+						if (in_bounds(x-3, y) and can_match(grid[y][x], grid[y][x-3])):
 							moves[(x-3, y, x-2, y)].append(grid[y][x])
-						if (in_bounds(x-2, y+1) and grid[y][x] == grid[y+1][x-2]):
+						if (in_bounds(x-2, y+1) and can_match(grid[y][x], grid[y+1][x-2])):
 							moves[(x-2, y+1, x-2, y)].append(grid[y][x])
 
-					if (grid[y][x] == grid[y-1][x]):
-						if (in_bounds(x-1, y-2) and grid[y][x] == grid[y-2][x-1]):
+					if (can_match(grid[y][x], grid[y-1][x])):
+						if (in_bounds(x-1, y-2) and can_match(grid[y][x], grid[y-2][x-1])):
 							moves[(x-1, y-2, x, y-2)].append(grid[y][x])
-						if (in_bounds(x, y-3) and grid[y][x] == grid[y-3][x]):
+						if (in_bounds(x, y-3) and can_match(grid[y][x], grid[y-3][x])):
 							moves[(x, y-3, x, y-2)].append(grid[y][x])
-						if (in_bounds(x+1, y-2) and grid[y][x] == grid[y-2][x+1]):
+						if (in_bounds(x+1, y-2) and can_match(grid[y][x], grid[y-2][x+1])):
 							moves[(x+1, y-2, x, y-2)].append(grid[y][x])
 
-						if (in_bounds(x-1, y+1) and grid[y][x] == grid[y+1][x-1]):
+						if (in_bounds(x-1, y+1) and can_match(grid[y][x], grid[y+1][x-1])):
 							moves[(x-1, y+1, x, y+1)].append(grid[y][x])
-						if (in_bounds(x, y+2) and grid[y][x] == grid[y+2][x]):
+						if (in_bounds(x, y+2) and can_match(grid[y][x], grid[y+2][x])):
 							moves[(x, y+2, x, y+1)].append(grid[y][x])
-						if (in_bounds(x+1, y+1) and grid[y][x] == grid[y+1][x+1]):
+						if (in_bounds(x+1, y+1) and can_match(grid[y][x], grid[y+1][x+1])):
 							moves[(x+1, y+1, x, y+1)].append(grid[y][x])
 
-					if (in_bounds(x-2, y) and grid[y][x] == grid[y][x-2]):
-						if (in_bounds(x-1, y-1) and grid[y][x] == grid[y-1][x-1]):
+					if (in_bounds(x-2, y) and can_match(grid[y][x], grid[y][x-2])):
+						if (in_bounds(x-1, y-1) and can_match(grid[y][x], grid[y-1][x-1])):
 							moves[(x-1, y-1, x-1, y)].append(grid[y][x])
-						if (in_bounds(x-1, y+1) and grid[y][x] == grid[y+1][x-1]):
+						if (in_bounds(x-1, y+1) and can_match(grid[y][x], grid[y+1][x-1])):
 							moves[(x-1, y+1, x-1, y)].append(grid[y][x])
 
-					if (in_bounds(x, y-2) and grid[y][x] == grid[y-2][x]):
-						if (in_bounds(x-1, y-1) and grid[y][x] == grid[y-1][x-1]):
+					if (in_bounds(x, y-2) and can_match(grid[y][x], grid[y-2][x])):
+						if (in_bounds(x-1, y-1) and can_match(grid[y][x], grid[y-1][x-1])):
 							moves[(x-1, y-1, x, y-1)].append(grid[y][x])
-						if (in_bounds(x+1, y-1) and grid[y][x] == grid[y-1][x+1]):
+						if (in_bounds(x+1, y-1) and can_match(grid[y][x], grid[y-1][x+1])):
 							moves[(x+1, y-1, x, y-1)].append(grid[y][x])
 		
 		# we couldn't find a move (mana drain animation clears the screen)
 		if (moves == []):
+			print("continuing")
 			continue
-
 
 		# prioritize a skull matching move
 		priorities = ["5", "s", "c", "e", "r", "y", "g", "b"]
-		moves = sorted(moves, key=lambda move: (len(moves[move]) + (1 if ("5" in moves[move] or "s" in moves[move]) else 0)), reverse = True)
+		moves = sorted(moves, key=lambda move: (len(moves[move]) + (.5 if ("5" in moves[move] or "s" in moves[move]) else 0)), reverse = True)
 		print(moves)
 		finalMove = moves[0]
 		# .5 to click center of tile
 		pyautogui.click(x = (finalMove[0] + .5) * GEM_WIDTH + minLeft, y = (finalMove[1] + .5) * GEM_HEIGHT + minTop)
 		pyautogui.click(x = (finalMove[2] + .5) * GEM_WIDTH + minLeft, y = (finalMove[3] + .5) * GEM_HEIGHT + minTop)
 
-		for i in grid:
-			print(i)
+		
 
