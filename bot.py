348b4653e7e602ca4f355978e0d92fcffa004f8c
Chuan Chen <15064893+riverchen99@users.noreply.github.com>
2019-08-30 03:48:16 -0400

    changed move priority calculation

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import pyautogui
 import os
 import itertools
+import collections
 
 GRID_WIDTH = 8
 GRID_HEIGHT = 8
@@ -44,70 +45,66 @@ while(True):
 				grid[gridY][gridX] = gemImageName[0]
 				gemGridLocations[gemImageName] += [{"y": gridY, "x": gridX}]
 
-		moves = []
+		moves = collections.defaultdict(list)
 		for y in range(1, GRID_HEIGHT):
 			for x in range(1, GRID_WIDTH):
 				if (grid[y][x] != "?"): # brute force a detect 3 move, inspired by https://github.com/akleemans/bejeweled-bot/blob/master/bejeweled-bot.py
+					# (from_x, from_y, to_x, to_y)
 					if (grid[y][x] == grid[y][x-1]):
 						if (in_bounds(x+1, y-1) and grid[y][x] == grid[y-1][x+1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-1, "x" : x+1}, "to" : {"y" : y, "x" : x+1}}]
+							moves[(x+1, y-1, x+1, y)].append(grid[y][x])
 						if (in_bounds(x+2, y) and grid[y][x] == grid[y][x+2]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y, "x" : x+2}, "to" : {"y" : y, "x" : x+1}}]
+							moves[(x+2, y, x+1, y)].append(grid[y][x])
 						if (in_bounds(x+1, y+1) and grid[y][x] == grid[y+1][x+1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+1, "x" : x+1}, "to" : {"y" : y, "x" : x+1}}]
+							moves[(x+1, y+1, x+1, y)].append(grid[y][x])
 
 						if (in_bounds(x-2, y-1) and grid[y][x] == grid[y-1][x-2]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-1, "x" : x-2}, "to" : {"y" : y, "x" : x-2}}]
+							moves[(x-2, y-1, x-2, y)].append(grid[y][x])
 						if (in_bounds(x-3, y) and grid[y][x] == grid[y][x-3]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y, "x" : x-3}, "to" : {"y" : y, "x" : x-2}}]
+							moves[(x-3, y, x-2, y)].append(grid[y][x])
 						if (in_bounds(x-2, y+1) and grid[y][x] == grid[y+1][x-2]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+1, "x" : x-2}, "to" : {"y" : y, "x" : x-2}}]
+							moves[(x-2, y+1, x-2, y)].append(grid[y][x])
 
 					if (grid[y][x] == grid[y-1][x]):
 						if (in_bounds(x-1, y-2) and grid[y][x] == grid[y-2][x-1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-2, "x" : x-1}, "to" : {"y" : y-2, "x" : x}}]
+							moves[(x-1, y-2, x, y-2)].append(grid[y][x])
 						if (in_bounds(x, y-3) and grid[y][x] == grid[y-3][x]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-3, "x" : x}, "to" : {"y" : y-2, "x" : x}}]
+							moves[(x, y-3, x, y-2)].append(grid[y][x])
 						if (in_bounds(x+1, y-2) and grid[y][x] == grid[y-2][x+1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-2, "x" : x+1}, "to" : {"y" : y-2, "x" : x}}]
+							moves[(x+1, y-2, x, y-2)].append(grid[y][x])
 
 						if (in_bounds(x-1, y+1) and grid[y][x] == grid[y+1][x-1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+1, "x" : x-1}, "to" : {"y" : y+1, "x" : x}}]
+							moves[(x-1, y+1, x, y+1)].append(grid[y][x])
 						if (in_bounds(x, y+2) and grid[y][x] == grid[y+2][x]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+2, "x" : x}, "to" : {"y" : y+1, "x" : x}}]
+							moves[(x, y+2, x, y+1)].append(grid[y][x])
 						if (in_bounds(x+1, y+1) and grid[y][x] == grid[y+1][x+1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+1, "x" : x+1}, "to" : {"y" : y+1, "x" : x}}]
+							moves[(x+1, y+1, x, y+1)].append(grid[y][x])
 
 					if (in_bounds(x-2, y) and grid[y][x] == grid[y][x-2]):
 						if (in_bounds(x-1, y-1) and grid[y][x] == grid[y-1][x-1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-1, "x" : x-1}, "to" : {"y" : y, "x" : x-1}}]
+							moves[(x-1, y-1, x-1, y)].append(grid[y][x])
 						if (in_bounds(x-1, y+1) and grid[y][x] == grid[y+1][x-1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y+1, "x" : x-1}, "to" : {"y" : y, "x" : x-1}}]
+							moves[(x-1, y+1, x-1, y)].append(grid[y][x])
 
 					if (in_bounds(x, y-2) and grid[y][x] == grid[y-2][x]):
 						if (in_bounds(x-1, y-1) and grid[y][x] == grid[y-1][x-1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-1, "x" : x+1}, "to" : {"y" : y-1, "x" : x}}]
+							moves[(x-1, y-1, x, y-1)].append(grid[y][x])
 						if (in_bounds(x+1, y-1) and grid[y][x] == grid[y-1][x+1]):
-							moves += [{"type": grid[y][x], "from" : {"y" : y-1, "x" : x+1}, "to" : {"y" : y-1, "x" : x}}]
+							moves[(x+1, y-1, x, y-1)].append(grid[y][x])
 		
 		# we couldn't find a move (mana drain animation clears the screen)
 		if (moves == []):
 			continue
 
-		print(moves)
 
 		# prioritize a skull matching move
-		finalMove = None
-		for move in moves:
-			print(move["type"])
-			if move["type"] == "s":
-				finalMove = move
-				break
-
-		finalMove = moves[0] if finalMove is None else finalMove
+		priorities = ["5", "s", "c", "e", "r", "y", "g", "b"]
+		moves = sorted(moves, key=lambda move: (len(moves[move]) + (1 if ("5" in moves[move] or "s" in moves[move]) else 0)), reverse = True)
+		print(moves)
+		finalMove = moves[0]
 		# .5 to click center of tile
-		pyautogui.click(x = (finalMove["from"]["x"] + .5) * GEM_WIDTH + minLeft, y = (finalMove["from"]["y"] + .5) * GEM_HEIGHT + minTop)
-		pyautogui.click(x = (finalMove["to"]["x"] + .5) * GEM_WIDTH + minLeft, y = (finalMove["to"]["y"] + .5) * GEM_HEIGHT + minTop)
+		pyautogui.click(x = (finalMove[0] + .5) * GEM_WIDTH + minLeft, y = (finalMove[1] + .5) * GEM_HEIGHT + minTop)
+		pyautogui.click(x = (finalMove[2] + .5) * GEM_WIDTH + minLeft, y = (finalMove[3] + .5) * GEM_HEIGHT + minTop)
 
 		for i in grid:
 			print(i)
